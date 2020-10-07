96dd4999bf8c8ce040bad1c35343cfb28ade4eb8
ali hussain <71531323+alihussain575@users.noreply.github.com>
2020-10-07 16:47:19 -0700

    calculator with switches

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let num1 = +prompt("Enter a number");
+console.log(num1);
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert( 'Too small' );
+    break;
+  case 4:
+    alert( 'Exactly!' );
+    break;
+  case 5:
+    alert( 'Too large' );
+    break;
+  default:
+    alert( "I don't know such values" );
+}
